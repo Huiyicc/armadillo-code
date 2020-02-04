4b12a57901548e5bedf59247479a9328f587e188
conrad <conrad@localhost.localdomain>
2020-02-04 17:33:21 +1000

    layout

--- include/armadillo_bits/diskio_meat.hpp
+++ include/armadillo_bits/diskio_meat.hpp
@@ -679,8 +679,8 @@ diskio::save_arma_ascii(const Mat<eT>& x, const std::string& final_name)
   std::ofstream f(tmp_name.c_str());
   
   bool save_okay = f.is_open();
-
-  if(save_okay)  
+  
+  if(save_okay)
     {
     save_okay = diskio::save_arma_ascii(x, f);
     
@@ -900,7 +900,7 @@ bool
 diskio::save_arma_binary(const Mat<eT>& x, std::ostream& f)
   {
   arma_extra_debug_sigprint();
-
+  
   f << diskio::gen_bin_header(x) << '\n';
   f << x.n_rows << ' ' << x.n_cols << '\n';
   
@@ -1129,7 +1129,7 @@ bool
 diskio::load_raw_ascii(Mat<eT>& x, const std::string& name, std::string& err_msg)
   {
   arma_extra_debug_sigprint();
-
+  
   std::fstream f;
   f.open(name.c_str(), std::fstream::in);
   
@@ -1328,7 +1328,7 @@ diskio::load_arma_ascii(Mat<eT>& x, std::istream& f, std::string& err_msg)
   arma_extra_debug_sigprint();
   
   std::streampos pos = f.tellg();
-    
+  
   bool load_okay = true;
   
   std::string f_header;
@@ -1824,7 +1824,7 @@ diskio::load_arma_binary(Mat<eT>& x, std::istream& f, std::string& err_msg)
   arma_extra_debug_sigprint();
   
   std::streampos pos = f.tellg();
-    
+  
   bool load_okay = true;
   
   std::string f_header;
@@ -1895,7 +1895,7 @@ diskio::pnm_skip_comments(std::istream& f)
   while( isspace(f.peek()) )
     {
     while( isspace(f.peek()) )  { f.get(); }
-  
+    
     if(f.peek() == '#')
       {
       while( (f.peek() != '\r') && (f.peek() != '\n') )  { f.get(); }
@@ -1946,15 +1946,15 @@ diskio::load_pgm_binary(Mat<eT>& x, std::istream& f, std::string& err_msg)
     uword f_n_rows = 0;
     uword f_n_cols = 0;
     int f_maxval = 0;
-  
+    
     diskio::pnm_skip_comments(f);
-  
+    
     f >> f_n_cols;
     diskio::pnm_skip_comments(f);
-  
+    
     f >> f_n_rows;
     diskio::pnm_skip_comments(f);
-  
+    
     f >> f_maxval;
     f.get();
     
@@ -2145,9 +2145,9 @@ diskio::load_hdf5_binary(Mat<eT>& x, const hdf5_name& spec, std::string& err_msg
         }
       
       arma_H5Dclose(dataset);
-    
+      
       arma_H5Fclose(fid);
-
+      
       if(load_okay == false)
         {
         err_msg = "unsupported or missing HDF5 data in ";
@@ -2157,7 +2157,7 @@ diskio::load_hdf5_binary(Mat<eT>& x, const hdf5_name& spec, std::string& err_msg
       {
       err_msg = "cannot open file ";
       }
-
+    
     return load_okay;
     }
   #else
@@ -2165,9 +2165,9 @@ diskio::load_hdf5_binary(Mat<eT>& x, const hdf5_name& spec, std::string& err_msg
     arma_ignore(x);
     arma_ignore(spec);
     arma_ignore(err_msg);
-
+    
     arma_stop_logic_error("Mat::load(): use of HDF5 must be enabled");
-
+    
     return false;
     }
   #endif
@@ -2187,7 +2187,7 @@ diskio::load_auto_detect(Mat<eT>& x, const std::string& name, std::string& err_m
     // We're currently using the C bindings for the HDF5 library, which don't support C++ streams
     if( arma_H5Fis_hdf5(name.c_str()) ) { return load_hdf5_binary(x, name, err_msg); }
   #endif
-
+  
   std::fstream f;
   f.open(name.c_str(), std::fstream::in | std::fstream::binary);
   
@@ -2390,23 +2390,23 @@ bool
 diskio::save_coord_ascii(const SpMat<eT>& x, const std::string& final_name)
   {
   arma_extra_debug_sigprint();
-
+  
   const std::string tmp_name = diskio::gen_tmp_name(final_name);
-
+  
   std::ofstream f(tmp_name.c_str());
-
+  
   bool save_okay = f.is_open();
-
+  
   if(save_okay)
     {
     save_okay = diskio::save_coord_ascii(x, f);
-
+    
     f.flush();
     f.close();
-
+    
     if(save_okay)  { save_okay = diskio::safe_rename(tmp_name, final_name); }
     }
-
+  
   return save_okay;
   }
 
@@ -2509,23 +2509,23 @@ bool
 diskio::save_arma_binary(const SpMat<eT>& x, const std::string& final_name)
   {
   arma_extra_debug_sigprint();
-
+  
   const std::string tmp_name = diskio::gen_tmp_name(final_name);
-
+  
   std::ofstream f(tmp_name.c_str(), std::fstream::binary);
-
+  
   bool save_okay = f.is_open();
-
+  
   if(save_okay)
     {
     save_okay = diskio::save_arma_binary(x, f);
-
+    
     f.flush();
     f.close();
-
+    
     if(save_okay)  { save_okay = diskio::safe_rename(tmp_name, final_name); }
     }
-
+  
   return save_okay;
   }
 
@@ -2974,18 +2974,18 @@ bool
 diskio::load_arma_binary(SpMat<eT>& x, const std::string& name, std::string& err_msg)
   {
   arma_extra_debug_sigprint();
-
+  
   std::ifstream f;
   f.open(name.c_str(), std::fstream::binary);
-
+  
   bool load_okay = f.is_open();
-
+  
   if(load_okay)
     {
     load_okay = diskio::load_arma_binary(x, f, err_msg);
     f.close();
     }
-
+  
   return load_okay;
   }
 
@@ -3082,7 +3082,7 @@ diskio::load_arma_binary(SpMat<eT>& x, std::istream& f, std::string& err_msg)
     load_okay = false;
     err_msg = "incorrect header in ";
     }
-
+  
   return load_okay;
   }
 
@@ -3592,7 +3592,7 @@ diskio::load_arma_ascii(Cube<eT>& x, std::istream& f, std::string& err_msg)
   arma_extra_debug_sigprint();
   
   std::streampos pos = f.tellg();
-    
+  
   bool load_okay = true;
   
   std::string f_header;
@@ -3767,15 +3767,15 @@ bool
 diskio::load_hdf5_binary(Cube<eT>& x, const hdf5_name& spec, std::string& err_msg)
   {
   arma_extra_debug_sigprint();
-
+  
   #if defined(ARMA_USE_HDF5)
     {
     hdf5_misc::hdf5_suspend_printing_errors hdf5_print_suspender;
-
+    
     bool load_okay = false;
-
+    
     hid_t fid = arma_H5Fopen(spec.filename.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
-
+    
     if(fid >= 0)
       {
       // MATLAB HDF5 dataset names are user-specified;
@@ -3802,60 +3802,60 @@ diskio::load_hdf5_binary(Cube<eT>& x, const hdf5_name& spec, std::string& err_ms
       if(dataset >= 0)
         {
         hid_t filespace = arma_H5Dget_space(dataset);
-
+        
         // This must be <= 3 due to our search rules.
         const int ndims = arma_H5Sget_simple_extent_ndims(filespace);
-
+        
         hsize_t dims[3];
         const herr_t query_status = arma_H5Sget_simple_extent_dims(filespace, dims, NULL);
-
+        
         // arma_check(query_status < 0, "Cube::load(): cannot get size of HDF5 dataset");
         if(query_status < 0)
           {
           err_msg = "cannot get size of HDF5 dataset in ";
-
+          
           arma_H5Sclose(filespace);
           arma_H5Dclose(dataset);
           arma_H5Fclose(fid);
-
+          
           return false;
           }
-
+        
         if (ndims == 1) { dims[1] = 1; dims[2] = 1; }  // Vector case; one row/colum, several slices
-        if (ndims == 2) { dims[2] = 1; } // Matrix case; one column, several rows/slices
-
+        if (ndims == 2) {              dims[2] = 1; }  // Matrix case; one column, several rows/slices
+        
         x.set_size(dims[2], dims[1], dims[0]);
-
+        
         // Now we have to see what type is stored to figure out how to load it.
         hid_t datatype = arma_H5Dget_type(dataset);
         hid_t mat_type = hdf5_misc::get_hdf5_type<eT>();
-
+        
         // If these are the same type, it is simple.
         if(arma_H5Tequal(datatype, mat_type) > 0)
           {
           // Load directly; H5S_ALL used so that we load the entire dataset.
           hid_t read_status = arma_H5Dread(dataset, datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, void_ptr(x.memptr()));
-
+          
           if(read_status >= 0) { load_okay = true; }
           }
         else
           {
           // Load into another array and convert its type accordingly.
           hid_t read_status = hdf5_misc::load_and_convert_hdf5(x.memptr(), dataset, datatype, x.n_elem);
-
+          
           if(read_status >= 0) { load_okay = true; }
           }
-
+        
         // Now clean up.
         arma_H5Tclose(datatype);
         arma_H5Tclose(mat_type);
         arma_H5Sclose(filespace);
         }
-
+      
       arma_H5Dclose(dataset);
-
+      
       arma_H5Fclose(fid);
-
+      
       if(load_okay == false)
         {
         err_msg = "unsupported or missing HDF5 data in ";
@@ -3865,7 +3865,7 @@ diskio::load_hdf5_binary(Cube<eT>& x, const hdf5_name& spec, std::string& err_ms
       {
       err_msg = "cannot open file ";
       }
-
+    
     return load_okay;
     }
   #else
@@ -3895,7 +3895,7 @@ diskio::load_auto_detect(Cube<eT>& x, const std::string& name, std::string& err_
     // We're currently using the C bindings for the HDF5 library, which don't support C++ streams
     if( arma_H5Fis_hdf5(name.c_str()) ) { return load_hdf5_binary(x, name, err_msg); }
   #endif
-
+  
   std::fstream f;
   f.open(name.c_str(), std::fstream::in | std::fstream::binary);
   
@@ -4094,13 +4094,13 @@ diskio::load_arma_binary(field<T1>& x, std::istream& f, std::string& err_msg)
     {
     uword f_n_rows;
     uword f_n_cols;
-  
+    
     f >> f_n_rows;
     f >> f_n_cols;
     
     x.set_size(f_n_rows, f_n_cols);
     
-    f.get();      
+    f.get();
     
     for(uword i=0; i<x.n_elem; ++i)
       {
@@ -4115,14 +4115,14 @@ diskio::load_arma_binary(field<T1>& x, std::istream& f, std::string& err_msg)
     uword f_n_rows;
     uword f_n_cols;
     uword f_n_slices;
-  
+    
     f >> f_n_rows;
     f >> f_n_cols;
     f >> f_n_slices;
     
     x.set_size(f_n_rows, f_n_cols, f_n_slices);
     
-    f.get();      
+    f.get();
     
     for(uword i=0; i<x.n_elem; ++i)
       {
@@ -4271,7 +4271,7 @@ diskio::load_std_string(field<std::string>& x, std::istream& f, std::string& err
     //f.seekg(start);
     
     x.set_size(f_n_rows, f_n_cols);
-  
+    
     for(uword row=0; row < x.n_rows; ++row)
     for(uword col=0; col < x.n_cols; ++col)
       {
@@ -4405,15 +4405,15 @@ diskio::load_ppm_binary(Cube<eT>& x, std::istream& f, std::string& err_msg)
     uword f_n_rows = 0;
     uword f_n_cols = 0;
     int f_maxval = 0;
-  
+    
     diskio::pnm_skip_comments(f);
-  
+    
     f >> f_n_cols;
     diskio::pnm_skip_comments(f);
-  
+    
     f >> f_n_rows;
     diskio::pnm_skip_comments(f);
-  
+    
     f >> f_maxval;
     f.get();
     
@@ -4466,7 +4466,7 @@ diskio::load_ppm_binary(Cube<eT>& x, std::istream& f, std::string& err_msg)
       load_okay = false;
       err_msg = "currently no code available to handle loading ";
       }
-      
+    
     if(f.good() == false)  { load_okay = false; }
     }
   else
@@ -4594,15 +4594,15 @@ diskio::load_ppm_binary(field<T1>& x, std::istream& f, std::string& err_msg)
     uword f_n_rows = 0;
     uword f_n_cols = 0;
     int f_maxval = 0;
-  
+    
     diskio::pnm_skip_comments(f);
-  
+    
     f >> f_n_cols;
     diskio::pnm_skip_comments(f);
-  
+    
     f >> f_n_rows;
     diskio::pnm_skip_comments(f);
-  
+    
     f >> f_maxval;
     f.get();
     
