9612b7bb4ff8c8f4880133ff7a15db8d592fac5a
conrad <conrad@localhost.localdomain>
2020-04-06 13:56:32 +1000

    simplification

--- include/armadillo_bits/op_diagmat_meat.hpp
+++ include/armadillo_bits/op_diagmat_meat.hpp
@@ -30,114 +30,17 @@ op_diagmat::apply(Mat<typename T1::elem_type>& out, const Op<T1, op_diagmat>& X)
   
   const Proxy<T1> P(X.m);
   
-  const uword n_rows = P.get_n_rows();
-  const uword n_cols = P.get_n_cols();
-  
-  const bool P_is_vec = (n_rows == 1) || (n_cols == 1);
-  
-  
-  if(P.is_alias(out) == false)
+  if(P.is_alias(out))
     {
-    if(P_is_vec)    // generate a diagonal matrix out of a vector
-      {
-      const uword N = (n_rows == 1) ? n_cols : n_rows;
-      
-      out.zeros(N, N);
-      
-      if(Proxy<T1>::use_at == false)
-        {
-        typename Proxy<T1>::ea_type P_ea = P.get_ea();
-        
-        for(uword i=0; i < N; ++i) { out.at(i,i) = P_ea[i]; }
-        }
-      else
-        {
-        if(n_rows == 1)
-          {
-          for(uword i=0; i < N; ++i) { out.at(i,i) = P.at(0,i); }
-          }
-        else
-          {
-          for(uword i=0; i < N; ++i) { out.at(i,i) = P.at(i,0); }
-          }
-        }
-      }
-    else   // generate a diagonal matrix out of a matrix
-      {
-      out.zeros(n_rows, n_cols);
-      
-      const uword N = (std::min)(n_rows, n_cols);
-      
-      for(uword i=0; i < N; ++i) { out.at(i,i) = P.at(i,i); }
-      }
+    Mat<eT> tmp;
+    
+    op_diagmat2::apply(tmp, P, 0, 0);
+    
+    out.steal_mem(tmp);
     }
-  else   // we have aliasing
+  else
     {
-    if(P_is_vec)   // generate a diagonal matrix out of a vector
-      {
-      const uword N = (n_rows == 1) ? n_cols : n_rows;
-      
-      podarray<eT> tmp(N);
-      eT* tmp_mem = tmp.memptr();
-      
-      if(Proxy<T1>::use_at == false)
-        {
-        typename Proxy<T1>::ea_type P_ea = P.get_ea();
-        
-        for(uword i=0; i < N; ++i) { tmp_mem[i] = P_ea[i]; }
-        }
-      else
-        {
-        if(n_rows == 1)
-          {
-          for(uword i=0; i < N; ++i) { tmp_mem[i] = P.at(0,i); }
-          }
-        else
-          {
-          for(uword i=0; i < N; ++i) { tmp_mem[i] = P.at(i,0); }
-          }
-        }
-      
-      out.zeros(N, N);
-      
-      for(uword i=0; i < N; ++i) { out.at(i,i) = tmp_mem[i]; }
-      }
-    else   // generate a diagonal matrix out of a matrix
-      {
-      const uword N = (std::min)(n_rows, n_cols);
-      
-      if( (Proxy<T1>::has_subview == false) && (Proxy<T1>::fake_mat == false) )
-        {
-        // NOTE: we have aliasing and it's not due to a subview, hence we're assuming that the output matrix already has the correct size
-        
-        for(uword i=0; i < n_cols; ++i)
-          {
-          if(i < N)
-            {
-            const eT val = P.at(i,i);
-            
-            arrayops::fill_zeros(out.colptr(i), n_rows);
-            
-            out.at(i,i) = val;
-            }
-          else
-            {
-            arrayops::fill_zeros(out.colptr(i), n_rows);
-            }
-          }
-        }
-      else
-        {
-        podarray<eT> tmp(N);
-        eT* tmp_mem = tmp.memptr();
-        
-        for(uword i=0; i < N; ++i)  { tmp_mem[i] = P.at(i,i); }
-        
-        out.zeros(n_rows, n_cols);
-        
-        for(uword i=0; i < N; ++i)  { out.at(i,i) = tmp_mem[i]; }
-        }
-      }
+    op_diagmat2::apply(out, P, 0, 0);
     }
   }
 
