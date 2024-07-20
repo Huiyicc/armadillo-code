f72efd7c619d0959f825f8781f4a8c059a3b3e9e
conrad <conrad@localhost.localdomain>
2024-07-20 17:16:45 +1000

    more graceful handling of unsupported data types

--- include/armadillo_bits/diskio_meat.hpp
+++ include/armadillo_bits/diskio_meat.hpp
@@ -1319,8 +1319,8 @@ diskio::save_hdf5_binary(const Mat<eT>& x, const hdf5_name& spec, std::string& e
     hid_t dataspace = H5Screate_simple(2, dims, NULL);   // treat the matrix as a 2d array dataspace
     hid_t datatype  = hdf5_misc::get_hdf5_type<eT>();
     
-    // If this returned something invalid, well, it's time to crash.
-    arma_check(datatype == -1, "Mat::save(): unknown datatype for HDF5");
+    // fail if we can't handle the datatype
+    if(datatype == -1)  { err_msg = "unknown datatype for HDF5"; return false; }
     
     // MATLAB forces the users to specify a name at save time for HDF5;
     // Octave will use the default of 'dataset' unless otherwise specified.
@@ -4016,8 +4016,8 @@ diskio::save_hdf5_binary(const Cube<eT>& x, const hdf5_name& spec, std::string& 
     hid_t dataspace = H5Screate_simple(3, dims, NULL);   // treat the cube as a 3d array dataspace
     hid_t datatype  = hdf5_misc::get_hdf5_type<eT>();
     
-    // If this returned something invalid, well, it's time to crash.
-    arma_check(datatype == -1, "Cube::save(): unknown datatype for HDF5");
+    // fail if we can't handle the datatype
+    if(datatype == -1)  { err_msg = "unknown datatype for HDF5"; return false; }
     
     // MATLAB forces the users to specify a name at save time for HDF5;
     // Octave will use the default of 'dataset' unless otherwise specified.
